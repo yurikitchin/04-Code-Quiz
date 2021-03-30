776fdd59cfb8cddec0442f4894cc0b7e733ab578
Yuri Kitchin <ykitchin@gmail.com>
2021-03-30 17:51:17 +0800

    added question object array

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@
 var startQuiz = document.getElementById("startQuiz");
 var timeElement = document.getElementById("timer");
 var secondsLeft = 75;
-var
+var question1
 
 //Timer Function
 function startTimer (){
@@ -27,4 +27,33 @@ function sendMessage () {
 
 //Quiz Function
 //Question 1
+//what is not a primitive data type
+var questions = [{
+    question: "Which is these is not a primative data type",
+    userChoice: ["booleans", "undefined", "variables", "number"],
+    answer: "variables",
+},
+{
+    question: "the Document method querySelector() Returns the first ____ within the document",
+    userChoice: ["variable", "css Selector", "node", "element"],
+    answer: "element",
+},
+{
+    question: "A useful tool that can be used during the development process to print content to the console is?",
+    userChoice: ["console log", "debugger", "Bash terminal", "conditional statment"],
+    answer: "console log",
+},
+{
+    question: "when a variable is declared inside of a function, where is that variable available?",
+    userChoice: ["that specific function", "window", "the child element", "Global Scope"],
+    answer: "that specific function",
+},
+{
+    question: "What can be stored within an object?",
+    userChoice: ["Primitive data types", "arrays", "methods", "all of the above"],
+    answer: "all of the above"
+}]
+
+
+//click event listener
 startQuiz.addEventListener('click', startTimer)
